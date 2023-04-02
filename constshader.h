a4fa848e350a383d53c359afe161496bc8811328
Christopher Schaefer <disks86@gmail.com>
2023-04-02 00:27:26 -0400

    Removed constexpr
    Removed constexpr.

--- constshader.h
+++ constshader.h
@@ -1,14 +1,9 @@
 #pragma once
 
 #include <vector>
-#include <unordered_map>
 #include <map>
-#include <stack>
-#include <memory>
-#include <fstream>
-#include <iostream>
-#include <string>
 #include <initializer_list>
+#include <variant>
 
 #ifdef __linux__ 
 #include <vulkan/vulkan.hpp>
@@ -39,9 +34,71 @@ Khronos at https://www.khronos.org/registry/spir-v/api/spir-v.xml.
 #define SPIR_V_GENERATORS_NUMBER 0x00000000
 
 namespace cs
-{
+{    
+    enum parameter_direction
+    {
+        in=0,
+        out=1
+    };
+
+    enum data_type
+    {
+        _bool=0,
+        _int=1,
+        _uint=2,
+        _float=3,
+        _double=4,
+        _bvec2=5,
+        _bvec3=6,
+        _bvec4=7,
+        _ivec2=8,
+        _ivec3=9,
+        _ivec4=10,
+        _uvec2=11,
+        _uvec3=12,
+        _uvec4=13,
+        _vec2=14,
+        _vec3=15,
+        _vec4=16,
+        _dvec2=17,
+        _dvec3=18,
+        _dvec4=19,
+        _mat2=20,
+        _mat3=21,
+        _mat4=22
+    };
+
+    struct layout_location_chunk
+    {
+        size_t mLocation;
+        parameter_direction mDirection;
+        data_type mType;
+        std::string mName;
+
+        layout_location_chunk(size_t location, parameter_direction direction, data_type type, const std::string& name)
+         : mLocation(location), mDirection(direction), mType(type), mName(name)
+        {
+            //TODO: implement
+        }
+
+        layout_location_chunk(const layout_location_chunk &obj)
+            : mLocation(obj.mLocation), mDirection(obj.mDirection), mType(obj.mType), mName(obj.mName)
+        {
+            
+        }
+
+        layout_location_chunk(layout_location_chunk &&obj)
+            : mLocation(obj.mLocation), mDirection(obj.mDirection), mType(obj.mType), mName(obj.mName)
+        { 
+            
+        }
+
+    };
+
+    using shader_chunk = std::variant<std::monostate, layout_location_chunk>;
 
     class shader
+        : public std::vector<shader_chunk>
     {
         private:
         uint32_t mNextId = 1;
@@ -55,8 +112,9 @@ namespace cs
             mNextId += numberToSkip;
         }
 
+        std::vector<uint32_t> mFunctionDefinitionInstructions;
         template<typename... Args>
-        void Push(spv::Op code, Args... args)
+        inline void Push(spv::Op code, Args... args)
         {
             Push(code, args...);
         }
@@ -65,16 +123,18 @@ namespace cs
         inline void Push(spv::Op code, std::initializer_list<T> arguments)
         {
             mFunctionDefinitionInstructions.push_back(Pack(arguments.size()+1, code)); //size,Type
-            for( auto argument : arguments )
+            for(auto argument : arguments)
             {
-                mFunctionDefinitionInstructions.push_back(argument1);
+                mFunctionDefinitionInstructions.push_back(argument);
             }
         }
 
         public:
-        constexpr shader()
-        {
 
+        inline shader& layout_location(size_t location, parameter_direction direction, data_type type, const std::string& name)
+        {
+            this->emplace_back(layout_location_chunk(location, direction, type, name));
+            return (*this);
         }
 
         inline vk::UniqueShaderModule Compile(vk::Device& device)
@@ -83,5 +143,4 @@ namespace cs
             return {};
         }
     };
-
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <variant>
 
 #include "constshader.h"
 
 int main()
-{
-    constexpr auto test = cs::shader();
+{    
+     auto shader = cs::shader()
+        .layout_location(0, cs::in, cs::_vec3, "inPos")
+        .layout_location(1, cs::in, cs::_vec3, "inColor");
 
     std::cout << "Hello World" << std::endl;
 }
